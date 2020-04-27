6fea05d501ba9bba2280c62c07e045dd2d88dd7d
Avik Bose <avikboseofficial1999@gmail.com>
2020-04-27 17:29:24 +0530

    fixed an issue

--- bounce the brick.py
+++ bounce the brick.py
@@ -7,15 +7,11 @@ PADDLE_COLOR = (255, 147, 79)
 BALL_COLOR = (34, 170, 161)
 RADIUS = 15
 BRICK_COLOR_1 = (178,34,34)
-
-
 pygame.init()
 pygame.display.set_caption("Brick Breaker")
 screen = pygame.display.set_mode(SCREEN_DIMENSIONS)
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(0)
-
-
 class Paddle:
 	"""docstring for Paddle"""
 	def __init__(self, X, Y, velX, width, height):
@@ -25,15 +21,10 @@ class Paddle:
 		self.width = width
 		self.height = height
 		self.paddleRect = pygame.draw.rect(screen, PADDLE_COLOR, (int(self.x), int(self.y), int(self.width), int(self.height)))
-		
-
 	def draw(self):
 		self.paddleRect = pygame.draw.rect(screen, PADDLE_COLOR, (int(self.x), int(self.y), int(self.width), int(self.height)))
 
 	def update_velocity(self, direction):
-
-		
-
 		if direction == "LEFT":
 			self.x -= self.velX
 		elif direction == "RIGHT":
@@ -45,17 +36,12 @@ class Paddle:
 			self.x = 0
 		if self.x + self.width > screenX:
 			self.x = screenX - self.width
-
-
-
 playerPaddle = Paddle(	X = screenX//2 - screenX*0.2//2, 
 						Y = 0.95 * screenY, 
 						velX = 15,
 						width = screenX*0.2,
 						height = screenY*0.02
 					)
-
-
 class Ball:
 	"""docstring for Ball"""
 	def __init__(self, x, y, velX, velY, radius):
@@ -67,37 +53,25 @@ class Ball:
 		self.currVelX = self.velX
 		self.currVelY = self.velY
 		self.bodyRect = pygame.draw.circle(screen, BALL_COLOR, (int(self.x), int(self.y)), int(self.radius))
-
 	def draw(self):
 		self.bodyRect = pygame.draw.circle(screen, BALL_COLOR, (int(self.x), int(self.y)), int(self.radius))
-
 	def update_position(self):
-
 		if self.x + self.radius > screenX:
 			self.currVelX = -self.velX
-
 		if self.x - self.radius < 0:
 			self.currVelX = self.velX
-
 		if self.y + self.radius > screenY or self.bodyRect.colliderect(playerPaddle.paddleRect) or self.bodyRect.colliderect(brick.brickRect):
 			self.currVelY = -self.velY
-
 		if self.y - self.radius < 0:
 			self.currVelY = self.velY
-
-
 		self.x += self.currVelX
 		self.y += self.currVelY
-
 	def set_velocity(self, velX, velY):
 		self.velX = velX
 		self.velY = velY
 		self.currVelX = self.velX
 		self.currVelY = self.velY
-
-
 gameBall = Ball(playerPaddle.x + playerPaddle.width//2, playerPaddle.y - RADIUS, 0, 0, RADIUS)
-
 class Brick(pygame.sprite.Sprite):
 	def __init__(self, X, Y,width, height):
 		pygame.sprite.Sprite.__init__(self)
@@ -106,40 +80,28 @@ class Brick(pygame.sprite.Sprite):
 		self.width = width
 		self.height = height
 		self.brickRect = pygame.draw.rect(screen, BRICK_COLOR_1, (int(self.x), int(self.y), int(self.width), int(self.height)))
-		
-
 	def draw(self):
 		self.paddleRect = pygame.draw.rect(screen, BRICK_COLOR_1, (int(self.x), int(self.y), int(self.width), int(self.height)))
-
-
 brick = Brick(	X = screenX//2 - 50//2,
 				Y = 0.2 * screenY,
 				width = 50,
 				height = 20
 			)
-
-
 def playGame():
-
-	
-
 	while True:
 		for event in pygame.event.get():
 
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
-
 		keyPressed = pygame.key.get_pressed()
-
 		if keyPressed[pygame.K_LEFT] or keyPressed[pygame.K_a]:
 			playerPaddle.update_velocity("LEFT")
 		elif keyPressed[pygame.K_RIGHT] or keyPressed[pygame.K_d]:
 			playerPaddle.update_velocity("RIGHT")
 		elif keyPressed[pygame.K_ESCAPE]:
 			pygame.quit()
-			sys.exit()
-				
+			sys.exit()				
 		gameBall.update_position()
 		screen.fill(BG_COLOR)
 		gameBall.draw()
@@ -147,7 +109,6 @@ def playGame():
 		brick.draw()
 		pygame.display.update()
 		clock.tick(70)
-
 def main():
 	while True:
 		for event in pygame.event.get():
@@ -155,17 +116,13 @@ def main():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
-
 		keyPressed = pygame.key.get_pressed()
-
 		if keyPressed[pygame.K_ESCAPE]:
 			pygame.quit()
 			sys.exit()
-
 		if keyPressed[pygame.K_SPACE]:
-			gameBall.set_velocity(10, 10)
+			gameBall.set_velocity(6, 6)
 			playGame()
-
 		gameBall.update_position()
 		screen.fill(BG_COLOR)
 		gameBall.draw()
@@ -173,7 +130,5 @@ def main():
 		brick.draw()
 		pygame.display.update()
 		clock.tick(70)
-
-
 if __name__ == '__main__':
 	main()
